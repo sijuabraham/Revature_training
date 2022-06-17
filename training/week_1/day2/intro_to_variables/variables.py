84ab5057bfe8c753f7972227a276c87c236629b6
Siju Abraham <sijumariyam@msn.com>
2022-06-17 17:20:18 -0400

    initial commit

---
+++ training/week_1/day2/intro_to_variables/variables.py
@@ -0,0 +1,11 @@
+abc = 100
+print(abc)
+x = 10
+y = 20
+print(x)
+print(y)
+
+print("hello")
+print(type(abc))
+print(type(x))
+print(type(y))
---
+++ training/week_1/day2/project_functions_3/functions.py
@@ -0,0 +1,34 @@
+
+def some_func(x, y):
+    value = x + y
+    return value
+
+
+a = some_func(5, 5)
+print(a)
+
+b = 10
+
+
+def some_func3():
+    b = 20
+    print(b)
+
+
+def some_func4():
+    print(b)
+
+
+print(b)
+
+some_func3()
+
+
+some_func4()
+
+
+
+
+
+
+
