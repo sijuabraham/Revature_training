5bc59daad3ad00737b9725cefedcb3844c992b3e
Siju Abraham <sijumariyam@msn.com>
2022-06-19 00:30:42 -0400

    Add intro_python_modules and intro-to-virtualenvironments

---
+++ training/week_1/day4/intro-python-modules/app.py
@@ -0,0 +1,15 @@
+import my_math_utility as m
+import numpy as np
+print(m.addition(10, 20))
+print(m.subtraction(40, 10))
+print(m.PI)
+
+
+a = np.arange(15).reshape(3, 5)
+print(a)
+
+import silly
+name = silly.name()
+print(f"my name is {name}")
+
+
---
+++ training/week_1/day4/intro-to-virtualenvironments/app.py
@@ -0,0 +1,5 @@
+import numpy as np
+import silly
+a = np.arange(15).reshape(3, 5)
+print(a)
+print(silly.name())
