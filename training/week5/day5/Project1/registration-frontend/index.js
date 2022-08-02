6321b2c1852af0c59bb6e01e6f117985e2652893
Siju Abraham <sijumariyam@msn.com>
2022-08-01 20:13:04 -0400

    add all the files

--- training/week5/day5/Project1/registration-frontend/index.js
+++ training/week5/day5/Project1/registration-frontend/index.js
@@ -52,7 +52,7 @@ async function get_all_details(){
         'headers': {'Content-Type': 'application/json'},
         'body': JSON.stringify({
             "username": usernameloginInput.value,
-            "password_1": passwordloginInput.value
+            "password": passwordloginInput.value
         })
     }) 
 
@@ -65,66 +65,66 @@ async function get_all_details(){
 
 // when submit button pressed
 
-registrationSubmitButton.addEventListener('click', async() =>{
+// registrationSubmitButton.addEventListener('click', async() =>{
 
-    let selectedRadioButton1;
-    for (let radioBtn1 of genderButtons){
-        if(radioBtn1.checked){
-            selectedRadioButton1 = radioBtn1
-            break;
-        }
-    }
+//     let selectedRadioButton1;
+//     for (let radioBtn1 of genderButtons){
+//         if(radioBtn1.checked){
+//             selectedRadioButton1 = radioBtn1
+//             break;
+//         }
+//     }
 
-    let selectedRadioButton2;
-    for(let radioBtn2 of roleButtons){
-        if(radioBtn2.checked){
-            selectedRadioButton2 = radioBtn2
-            break;
-        }
-    }
+//     let selectedRadioButton2;
+//     for(let radioBtn2 of roleButtons){
+//         if(radioBtn2.checked){
+//             selectedRadioButton2 = radioBtn2
+//             break;
+//         }
+//     }
 
-    let res = await fetch('http://127.0.0.1:8080/users', {
-        'credentials': 'include', // very important to make sure that login and everything else works, because
-        // 'credentials': 'include' is the option that will tell the browser to include cookies as part of the request
-        'method': 'POST',
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'body': JSON.stringify({
-            "username": usernameInput.value,
-            "password_1": passwordInput.value,
-            "first_name": firstNameInput.value,
-            "last_name": lastNameInput.value,
-            "gender": selectedRadioButton1.value,
-            "phone_number": phoneInput.value,
-            "email_address": emailInput.value,
-            "role_1": selectedRadioButton2.value
-        })
-    })
+//     let res = await fetch('http://127.0.0.1:8080/users', {
+//         'credentials': 'include', // very important to make sure that login and everything else works, because
+//         // 'credentials': 'include' is the option that will tell the browser to include cookies as part of the request
+//         'method': 'POST',
+//         'headers': {
+//             'Content-Type': 'application/json'
+//         },
+//         'body': JSON.stringify({
+//             "username": usernameInput.value,
+//             "password_1": passwordInput.value,
+//             "first_name": firstNameInput.value,
+//             "last_name": lastNameInput.value,
+//             "gender": selectedRadioButton1.value,
+//             "phone_number": phoneInput.value,
+//             "email_address": emailInput.value,
+//             "role_1": selectedRadioButton2.value
+//         })
+//     })
 
 
-    if (res.status == 201) {
+//     if (res.status == 201) {
 
-        window.location.href = '/success.html'
+//         window.location.href = '/success.html'
 
-    } else if (res.status == 400) {
-        let data = await res.json();
+//     } else if (res.status == 400) {
+//         let data = await res.json();
         
-        let registrationErrorMessagesDiv = document.getElementById('registration-error-messages')
-        registrationErrorMessagesDiv.innerHTML = '';
+//         let registrationErrorMessagesDiv = document.getElementById('registration-error-messages')
+//         registrationErrorMessagesDiv.innerHTML = '';
 
-        let errorMessages = data.messages;
-        for(let errorMessage of errorMessages ){
-            let errorElement = document.createElement('p');
-            errorElement.innerHTML = errorMessage;
-            errorElement.style.color = 'red';
-            errorElement.style.fontWeight = 'bold';
+//         let errorMessages = data.messages;
+//         for(let errorMessage of errorMessages ){
+//             let errorElement = document.createElement('p');
+//             errorElement.innerHTML = errorMessage;
+//             errorElement.style.color = 'red';
+//             errorElement.style.fontWeight = 'bold';
 
 
-            registrationErrorMessagesDiv.appendChild(errorElement);
-        }
+//             registrationErrorMessagesDiv.appendChild(errorElement);
+//         }
         
-    }
+//     }
 
 
-});
+// });
